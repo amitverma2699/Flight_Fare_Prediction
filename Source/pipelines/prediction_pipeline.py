19a34158a438778cf7fa1dae3d061fb52cbdf20c
amitverma2699 <amit99bhama@gmail.com>
2025-02-11 17:40:00 +0530

    update File

--- Source/pipelines/prediction_pipeline.py
+++ Source/pipelines/prediction_pipeline.py
@@ -1,78 +1,121 @@
 import os
 import sys
 import pandas as pd
+import numpy as np
 from Source.logger import logging
 from Source.exception import CustomException
 from Source.utils.utils import load_object
 
-class PredictPipeline:
-    def __init__(self):
-        pass
+class FlightData:
+    """
+    Custom class to handle user input data for flight fare prediction.
+    """
 
-    def predict(self,features):
+    def __init__(self, Airline, Source, Destination, Route, Dep_Time, Arrival_Time, Duration, Total_Stops, Additional_Info, Date_of_Journey):
+        self.Airline = Airline
+        self.Source = Source
+        self.Destination = Destination
+        self.Route = Route
+        self.Dep_Time = Dep_Time
+        self.Arrival_Time = Arrival_Time
+        self.Duration = Duration
+        self.Total_Stops = Total_Stops
+        self.Additional_Info = Additional_Info
+        self.Date_of_Journey = Date_of_Journey
+
+    def get_data(self):
+        """
+        Returns input data as a dictionary.
+        """
         try:
-            preprocessor_path=os.path.join("Artifacts","preprocessor.pkl")
-            model_path=os.path.join("Artifacts","model.pkl")
-
-            preprocessor=load_object(preprocessor_path)
-            model=load_object(model_path)
-
-            scaled_data=preprocessor.transform(features)
-            prediction=model.predict(scaled_data)
-
-            return prediction
-
+            flight_details = {
+                "Airline": self.Airline,
+                "Source": self.Source,
+                "Destination": self.Destination,
+                "Route": self.Route,
+                "Dep_Time": self.Dep_Time,
+                "Arrival_Time": self.Arrival_Time,
+                "Duration": self.Duration,
+                "Total_Stops": self.Total_Stops,
+                "Additional_Info": self.Additional_Info,
+                "Date_of_Journey": self.Date_of_Journey
+            }
+            return flight_details
         except Exception as e:
-            logging.info("Error occured in Prediction Pipeline")
-            raise CustomException(e,sys)
-        
-
-class CustomData:
-    def __init__(self,
-                 carat:float,
-                 depth:float,
-                 table:float,
-                 x:float,
-                 y:float,
-                 z:float,
-                 cut:str,
-                 color:str,
-                 clarity:str,
-                 ):
-        self.carat=carat
-        self.depth=depth
-        self.table=table
-        self.x=x
-        self.y=y
-        self.z=z
-        self.cut=cut
-        self.color=color
-        self.clarity=clarity
+            logging.error("Error occurred while getting flight data.")
+            raise CustomException(e, sys)
 
     def get_data_as_dataframe(self):
+        """
+        Converts input data into a Pandas DataFrame.
+        """
         try:
-            custom_data_input_dict={
-                "carat":[self.carat],
-                "depth":[self.depth],
-                "table":[self.table],
-                "x":[self.x],
-                "y":[self.y],
-                "z":[self.z],
-                "cut":[self.cut],
-                "color":[self.color],
-                "clarity":[self.clarity]
-            }
+            flight_details = self.get_data()
+            df = pd.DataFrame([flight_details])
+            return df
+        except Exception as e:
+            logging.error("Error occurred while converting data to DataFrame.")
+            raise CustomException(e, sys)
 
-            df=pd.DataFrame(custom_data_input_dict)
-            logging.info("DataFrame Created")
+class FlightFarePrediction:
+    def __init__(self):
+        try:
+            self.model_path = os.path.join("Artifacts", "model.pkl")
+            self.preprocessor_path = os.path.join("Artifacts", "preprocessor.pkl")
+
+            logging.info("Loading saved model and preprocessor...")
+            self.model = load_object(self.model_path)
+            self.preprocessor = load_object(self.preprocessor_path)
+
+        except Exception as e:
+            logging.error("Error occurred while loading model or preprocessor.")
+            raise CustomException(e, sys)
+
+    def preprocess_input(self, input_data: pd.DataFrame):
+        """
+        Preprocess user input to match the model's training data.
+        """
+        try:
+            df = input_data.copy()
+
+            # Feature Engineering (same as in `DataTransformation`)
+            df['Journey_Day'] = pd.to_datetime(df['Date_of_Journey'], format='%d/%m/%Y').dt.day
+            df['Journey_Month'] = pd.to_datetime(df['Date_of_Journey'], format='%d/%m/%Y').dt.month
+            df.drop('Date_of_Journey', axis=1, inplace=True)
+
+            df['Dep_Hour'] = pd.to_datetime(df['Dep_Time']).dt.hour
+            df['Dep_Minute'] = pd.to_datetime(df['Dep_Time']).dt.minute
+            df.drop('Dep_Time', axis=1, inplace=True)
+
+            df['Arrival_Hour'] = pd.to_datetime(df['Arrival_Time']).dt.hour
+            df['Arrival_Minute'] = pd.to_datetime(df['Arrival_Time']).dt.minute
+            df.drop('Arrival_Time', axis=1, inplace=True)
+
+            df["Duration_Hour"] = df["Duration"].str.extract(r'(\d+)h')[0].astype(float)
+            df["Duration_Minute"] = df["Duration"].str.extract(r'(\d+)m')[0].fillna(0).astype(float)
+            df.drop(['Duration'], axis=1, inplace=True)
+
+            logging.info("User input preprocessed successfully.")
             return df
 
         except Exception as e:
-            logging.info("Error occured in CustomData")
-            raise CustomException(e,sys)
-        
-                 
-                 
-                 
-                 
-                 
+            logging.error("Error occurred while preprocessing user input.")
+            raise CustomException(e, sys)
+
+    def predict(self, input_data: pd.DataFrame):
+        """
+        Predict flight fare based on user input.
+        """
+        try:
+            processed_data = self.preprocess_input(input_data)
+            transformed_data = self.preprocessor.transform(processed_data)
+            
+            prediction = self.model.predict(transformed_data)
+            predicted_price = round(prediction[0], 2)
+
+            logging.info(f"Prediction Successful! Estimated Flight Price: {predicted_price}")
+            return predicted_price
+
+        except Exception as e:
+            logging.error("Error occurred while making prediction.")
+            raise CustomException(e, sys)
